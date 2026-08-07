55684292f815cea5d7df2157a2aaffcc3ce1ddd9
drawing1324-dot <drawing1324@gmail.com>
2026-08-08 03:26:30 +0700

    Update publisher.py

--- modules/blogger/publisher.py
+++ modules/blogger/publisher.py
@@ -182,7 +182,7 @@ class BloggerPublisher:
 
                     fetchBodies=False,
 
-                    orderBy="updated"
+                    orderBy="UPDATED"
                 )
             )
 
